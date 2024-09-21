1e03af2be3843d38c80410b8ea48a53fe9015724
Jeferson Santos <jefersonsann@hotmail.com>
2024-09-21 15:43:39 -0300

    refactor: dialog simplified

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -5,12 +5,10 @@ import * as React from "react";
 
 interface DialogProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  onClose: () => void;
-  onOk: () => void;
   type: string;
 }
 
-const Dialog = ({ children, onClose, onOk, type }: DialogProps) => {
+const Dialog = ({ children, type }: DialogProps) => {
   const searchParams = useSearchParams();
   const dialogRef = React.useRef<null | HTMLDialogElement>(null);
   const showDialog = searchParams.get("showDialog");
@@ -22,20 +20,8 @@ const Dialog = ({ children, onClose, onOk, type }: DialogProps) => {
     dialogRef.current?.showModal();
   }, [showDialog, type]);
 
-  const closeDialog = () => {
-    dialogRef.current?.close();
-    onClose();
-  };
-
-  const clickOk = () => {
-    onOk();
-    closeDialog();
-  };
-
   return (
     <dialog ref={dialogRef}>
-      <span onClick={closeDialog} />
-      <span onClick={clickOk} />
       <div>{children}</div>
     </dialog>
   );
